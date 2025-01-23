9ea244aa93b1a4d9bfab17ee64b0e2009d987da3
rhuanyago <rhuan.yago2014@live.com>
2025-01-23 11:21:12 -0300

    chore: add initial tables

--- app/_components/sidebar.tsx
+++ app/_components/sidebar.tsx
@@ -1,14 +1,26 @@
+import { BoxIcon, LayoutGrid, ShoppingBasketIcon } from "lucide-react";
+import SidebarButton from "./sidebar-button";
+
 const Sidebar = () => {
   return (
     <div className="w-64 bg-white">
       <div className="px-8 py-6">
-        <h1 className="text-2xl font-bold">Stockly</h1>
+        <h1 className="text-2xl font-bold">STOCKLY</h1>
       </div>
 
       <div className="flex flex-col gap-2 p-2">
-        <button className="px-6 py-3">Dashboard</button>
-        <button className="px-6 py-3">Produtos</button>
-        <button className="px-6 py-3">Vendas</button>
+        <SidebarButton href="/">
+          <LayoutGrid size={20} />
+          Dashboard
+        </SidebarButton>
+        <SidebarButton href="/products">
+          <BoxIcon size={20} />
+          Produtos
+        </SidebarButton>
+        <SidebarButton href="/sales">
+          <ShoppingBasketIcon size={20} />
+          Vendas
+        </SidebarButton>
       </div>
     </div>
   );
