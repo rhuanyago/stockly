22cb2d1ec9dbd5ef957cd4ab720d56dfcf221aff
rhuanyago <rhuan.yago2014@live.com>
2025-01-24 01:00:46 -0300

    feat: add initial sale create component

--- app/sales/page.tsx
+++ app/sales/page.tsx
@@ -1,5 +1,37 @@
-const SalesPage = () => {
-  return ( <h1>Sales Page</h1> );
-}
- 
+import { Button } from "../_components/ui/button";
+import { ComboboxOption } from "../_components/ui/combobox";
+import { Sheet, SheetTrigger } from "../_components/ui/sheet";
+import { getProducts } from "../_data-access/product/get-products";
+import UpsertSheetContent from "./_componentes/upsert-sheet-content";
+
+const SalesPage = async () => {
+  const products = await getProducts();
+  const productOptions: ComboboxOption[] = products.map((product) => ({
+    value: product.id,
+    label: product.name,
+  }));
+
+  return (
+    <div className="m-8 w-full space-y-8 rounded-lg bg-white p-8 shadow">
+      <div className="flex w-full items-center justify-between">
+        <div className="space-y-1">
+          <span className="text-xs font-semibold text-slate-500">
+            Gestão de Vendas
+          </span>
+          <h2 className="text-xl font-bold">Vendas</h2>
+        </div>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button>Nova Venda</Button>
+          </SheetTrigger>
+          <UpsertSheetContent
+            productOptions={productOptions}
+            products={products}
+          />
+        </Sheet>
+      </div>
+    </div>
+  );
+};
+
 export default SalesPage;
