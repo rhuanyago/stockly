dd4b9191389b523a71eeb688f76880f2edd9de77
rhuanyago <rhuan.yago2014@live.com>
2025-01-23 23:16:09 -0300

    feat: ensure product can be updated

--- app/products/_components/upsert-dialog-content.tsx
+++ app/products/_components/upsert-dialog-content.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { createProduct } from "@/app/_actions/products/create-product";
+import { upsertProduct } from "@/app/_actions/products/upsert-product";
 import {
-  createProductSchema,
-  CreateProductSchema,
-} from "@/app/_actions/products/create-product/schema";
+  upsertProductSchema,
+  UpsertProductSchema,
+} from "@/app/_actions/products/upsert-product/schema";
 import { Button } from "@/app/_components/ui/button";
 import {
   DialogClose,
@@ -30,38 +30,46 @@ import { NumericFormat } from "react-number-format";
 import { toast } from "sonner";
 
 interface UpsertProductDialogContentProps {
+  defaultValues?: UpsertProductSchema;
   onSuccess?: () => void;
 }
 
 const UpsertProductDialogContent = ({
+  defaultValues,
   onSuccess,
 }: UpsertProductDialogContentProps) => {
-  const form = useForm<CreateProductSchema>({
+  const form = useForm<UpsertProductSchema>({
     shouldUnregister: true,
-    resolver: zodResolver(createProductSchema),
-    defaultValues: {
+    resolver: zodResolver(upsertProductSchema),
+    defaultValues: defaultValues ?? {
       name: "",
       price: 0,
       stock: 1,
     },
   });
 
-  const onSubmit = async (data: CreateProductSchema) => {
+  const onSubmit = async (data: UpsertProductSchema) => {
     try {
-      await createProduct(data);
+      await upsertProduct({ ...data, id: defaultValues?.id });
       onSuccess?.();
-      toast.success("Produto criado com sucesso!");
+      if (defaultValues) {
+        toast.success("Produto atualizado com sucesso.");
+        return;
+      }
+      toast.success("Produto criado com sucesso.");
     } catch (error) {
       toast.error("Ocorreu um erro ao criar o produto.");
     }
   };
+
+  const isEditing = !!defaultValues;
 
   return (
     <DialogContent>
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
           <DialogHeader>
-            <DialogTitle>Novo Produto</DialogTitle>
+            <DialogTitle>{isEditing ? "Editar" : "Criar"} produto</DialogTitle>
             <DialogDescription>
               Preencha os campos abaixo para adicionar um novo produto.
             </DialogDescription>
@@ -137,7 +145,7 @@ const UpsertProductDialogContent = ({
               {form.formState.isSubmitting && (
                 <Loader2Icon className="animate-spin" size={16} />
               )}
-              Adicionar Produto
+              {isEditing ? "Salvar" : "Criar"} produto
             </Button>
           </DialogFooter>
         </form>
